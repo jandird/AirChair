aae53f8e572647e8e4d785b3ae1218bbc2073016
jandird <jandird@mcmaster.ca>
2019-11-12 14:35:06 -0500

    Adding DarkFlowTest file which uses YOLO algo

---
+++ Prototyping/TensorFlow/DarkFlowTest.py
@@ -0,0 +1,28 @@
+from darkflow.net.build import TFNet
+import cv2
+from PIL import Image
+
+
+if __name__ == '__main__':
+
+    options = {"model": "./cfg/yolo.cfg", "load": "./bin/yolo.weights", "threshold": 0.1}
+
+    tfnet = TFNet(options)
+
+    imgcv = cv2.imread("./resources/test-img/test5.jpg")
+    results = tfnet.return_predict(imgcv)
+    print(results)
+
+    for result in results:
+        cv2.rectangle(imgcv,
+                      (result["topleft"]["x"], result["topleft"]["y"]),
+                      (result["bottomright"]["x"],result["bottomright"]["y"]),
+                      (0, 255, 0), 4)
+
+        text_x, text_y = result["topleft"]["x"] - 10, result["topleft"]["y"] - 10
+
+        cv2.putText(imgcv, result["label"], (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+    im = Image.fromarray(imgcv)
+    im.save('./resources/analyzed-img/analyzed5.png')
+
---
+++ Prototyping/TensorFlow/bin/README.md
@@ -0,0 +1,2 @@
+Download the YOLOv2 608x608 weights here and name it yolo.weights
+Download the file from: https://pjreddie.com/darknet/yolo/
