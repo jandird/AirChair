2323f8bfb79dd8d36248692f531aa5274557c4c6
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2019-11-17 14:06:19 -0500

    occupied chair calculation

--- Prototyping/TensorFlow/DarkFlowTest.py
+++ Prototyping/TensorFlow/DarkFlowTest.py
@@ -10,22 +10,58 @@ if __name__ == '__main__':
     tfnet = TFNet(options)
 
     # INPUT IMAGE HERE
-    imgcv = cv2.imread("./resources/test-img/test5.jpg")
+    imgcv = cv2.imread("./resources/test-img/IMG_1287.jpg")
     results = tfnet.return_predict(imgcv)
-    print(results)
+    # print(results)
+
+    chairs = []
+    people = []
 
     for result in results:
-        cv2.rectangle(imgcv,
-                      (result["topleft"]["x"], result["topleft"]["y"]),
-                      (result["bottomright"]["x"],result["bottomright"]["y"]),
-                      (0, 255, 0), 4)
+        if (result["confidence"] > 0.24):
+            print(result)
+            if (result["label"] == 'chair'):
+                # rectCentre = (int(round(result["topleft"]["x"]+result["bottomright"]["x"])/2), int(round((result["topleft"]["y"]+result["bottomright"]["y"])/2)))
+                # print(rectCentre)
+                # cv2.circle(imgcv, rectCentre, 30,(0, 0, 255), -1)
+                chairs.append(result)
+            if (result["label"] == 'person'):
+                # rectCentre = (int(round(result["topleft"]["x"]+result["bottomright"]["x"])/2), int(round((result["topleft"]["y"]+result["bottomright"]["y"])/2)))
+                # print(rectCentre)
+                # cv2.circle(imgcv, rectCentre, 30,(225, 0, 0), -1)
+                people.append(result)
 
-        text_x, text_y = result["topleft"]["x"] - 10, result["topleft"]["y"] - 10
+            cv2.rectangle(imgcv,
+                        (result["topleft"]["x"], result["topleft"]["y"]),
+                        (result["bottomright"]["x"],result["bottomright"]["y"]),
+                        (0, 255, 0), 4)
 
-        cv2.putText(imgcv, result["label"], (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+            text_x, text_y = result["topleft"]["x"] - 10, result["topleft"]["y"] - 10
+
+            cv2.putText(imgcv, result["label"], (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+    print("number of chairs: " + str(len(chairs)))
+    print("number of people: " + str(len(people)))
+
+    # Calculate if a chair is occupied by a person
+    for chair in chairs:
+        rectCentre = (int(round(chair["topleft"]["x"]+chair["bottomright"]["x"])/2), int(round((chair["topleft"]["y"]+chair["bottomright"]["y"])/2)));
+        occupiedFlag = False
+        for person in people:
+            perRectCentre  = (int(round(person["topleft"]["x"]+person["bottomright"]["x"])/2), int(round((person["topleft"]["y"]+person["bottomright"]["y"])/2)));
+            if (abs(rectCentre[0] - perRectCentre[0]) < 200 and abs(rectCentre[1] - perRectCentre[1]) < 200 ):
+                occupiedFlag = True
+                break
+        if occupiedFlag:
+            cv2.circle(imgcv, rectCentre, 30,(225, 0, 0), -1)
+        else:
+            cv2.circle(imgcv, rectCentre, 30,(0, 225, 0), -1)
 
     im = Image.fromarray(imgcv)
 
     # OUTPUT IMAGE HERE
     im.save('./resources/analyzed-img/analyzed5.png')
 
+
+
+  
