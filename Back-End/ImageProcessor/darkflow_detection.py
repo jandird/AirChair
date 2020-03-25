5062c6a736721c44742b3d4f6934c08d97d4e65c
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2020-03-25 15:41:40 -0400

    person workaround
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- Back-End/ImageProcessor/darkflow_detection.py
+++ Back-End/ImageProcessor/darkflow_detection.py
@@ -3,7 +3,7 @@ import cv2
 import json
 from PIL import Image
 
-options = {"model": "./cfg/yolo.cfg", "load": "./bin/yolo.weights", "threshold": 0.25}
+options = {"model": "./cfg/yolo.cfg", "load": "./bin/yolo.weights", "threshold": 0.35}
 tfnet = TFNet(options)
 
 
@@ -19,7 +19,9 @@ def analyze_img(img, count):
                  'seats': {},
                  'door': {"corner": "BL", "opens": "L"}}
     seatsDictArr = []
+    chairDictArr = []
     tablesDictArr = []
+
 
     for result in results:
         if (result["confidence"] > 0.25):
@@ -56,29 +58,74 @@ def analyze_img(img, count):
     tablesDictArr = sorted(tablesDictArr, key=lambda i: (i['xmin'], i['ymin']))
 
     # Calculate if a chair is occupied by a person
+    for person in people:
+        perRectCentre = (int(round(person["topleft"]["x"] + person["bottomright"]["x"]) / 2),
+                             int(round((person["topleft"]["y"] + person["bottomright"]["y"]) / 2)));
+        personDict = {}
+        personDict['xcoord'] = perRectCentre[0]
+        personDict['ycoord'] = perRectCentre[1]
+
+        foundSeatFlag = False
+
+        #see if any chair matches up with this person
+        for chair in chairs:
+            seatsDict = {}
+            rectCentre = (int(round(chair["topleft"]["x"] + chair["bottomright"]["x"]) / 2),
+                    int(round((chair["topleft"]["y"] + chair["bottomright"]["y"]) / 2)))
+            seatsDict['xcoord'] = rectCentre[0]
+            seatsDict['ycoord'] = rectCentre[1]
+            if (abs(rectCentre[0] - perRectCentre[0]) < 150 and abs(rectCentre[1] - perRectCentre[1]) < 150):
+                occupiedFlag = True
+                cv2.circle(imgcv, rectCentre, 30, (225, 0, 0), -1)
+                seatsDict['occupied'] = "true"
+                foundSeatFlag = True
+                chairDictArr.append(seatsDict)
+                seatsDictArr.append(seatsDict)
+                break
+        
+        # no chair matches up, create a chair and mark as occupied
+        if (foundSeatFlag == False):
+            personDict['occupied'] = "true"
+            cv2.circle(imgcv, perRectCentre, 30, (225, 0, 0), -1)
+            seatsDictArr.append(personDict) 
+
     for chair in chairs:
         seatsDict = {}
         rectCentre = (int(round(chair["topleft"]["x"] + chair["bottomright"]["x"]) / 2),
                       int(round((chair["topleft"]["y"] + chair["bottomright"]["y"]) / 2)))
-        occupiedFlag = False
-        for person in people:
-            perRectCentre = (int(round(person["topleft"]["x"] + person["bottomright"]["x"]) / 2),
-                             int(round((person["topleft"]["y"] + person["bottomright"]["y"]) / 2)));
-            if (abs(rectCentre[0] - perRectCentre[0]) < 150 and abs(rectCentre[1] - perRectCentre[1]) < 150):
-                occupiedFlag = True
-                break
-
         seatsDict['xcoord'] = rectCentre[0]
         seatsDict['ycoord'] = rectCentre[1]
-
-        if occupiedFlag:
-            cv2.circle(imgcv, rectCentre, 30, (225, 0, 0), -1)
-            seatsDict['occupied'] = "true"
-        else:
+        if seatsDict not in chairDictArr:
             cv2.circle(imgcv, rectCentre, 30, (0, 225, 0), -1)
+            seatsDict['occupied'] = "false"
             seatsDict['occupied'] = "false"
 
         seatsDictArr.append(seatsDict)
+
+        # seatsDict = {}
+        # rectCentre = (int(round(chair["topleft"]["x"] + chair["bottomright"]["x"]) / 2),
+        #               int(round((chair["topleft"]["y"] + chair["bottomright"]["y"]) / 2)))
+        # occupiedFlag = False
+        # for person in people:
+        #     perRectCentre = (int(round(person["topleft"]["x"] + person["bottomright"]["x"]) / 2),
+        #                      int(round((person["topleft"]["y"] + person["bottomright"]["y"]) / 2)))
+        #     # check if person has already been included in the dictionary
+
+        #     if (abs(rectCentre[0] - perRectCentre[0]) < 150 and abs(rectCentre[1] - perRectCentre[1]) < 150):
+        #         occupiedFlag = True
+        #         break
+
+        # seatsDict['xcoord'] = rectCentre[0]
+        # seatsDict['ycoord'] = rectCentre[1]
+
+        # if occupiedFlag:
+        #     cv2.circle(imgcv, rectCentre, 30, (225, 0, 0), -1)
+        #     seatsDict['occupied'] = "true"
+        # else:
+        #     cv2.circle(imgcv, rectCentre, 30, (0, 225, 0), -1)
+        #     seatsDict['occupied'] = "false"
+
+        # seatsDictArr.append(seatsDict)
 
     seatsDictArr = sorted(seatsDictArr, key=lambda i: (i['xcoord'], i['ycoord']))
 
