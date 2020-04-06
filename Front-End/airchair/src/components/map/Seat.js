f6ccedd11b7e9453ae14bd8cd50493228fafd775
jandird <jandird@mcmaster.ca>
2020-04-06 19:44:48 -0400

    Added square seats for occupied & changed name to airchair

--- Front-End/airchair/src/components/map/Seat.js
+++ Front-End/airchair/src/components/map/Seat.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import {Circle} from 'react-konva';
+import {Circle, Rect} from 'react-konva';
 
 function Seat (props){
 
     let color;
+    let seat;
     if (props.occupied === "true"){
         color = "#ff1744";
+        seat = <Rect x={props.xcoord - 75} y={props.ycoord -75} width={150} height={150} fill={color} cornerRadius={25} />
     }
     else {
         color = "#1de9b6";
+        seat =  <Circle x={props.xcoord} y={props.ycoord} radius={75} fill={color} />
     }
 
     return (
-        <Circle x={props.xcoord} y={props.ycoord} radius={75} fill={color} />
+        seat
     );
 }
 
